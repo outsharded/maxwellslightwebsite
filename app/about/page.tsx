ff5abc073b65e8b6b25002ad36cc2f8fbee6d627
outsharded <fab_tec@icloud.com>
2023-12-29 01:18:32 +0000

    about

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -3,14 +3,14 @@ import React from 'react';
 const About = () => {
   return (
     <div className="bg-black text-white min-h-screen p-8">
-      <h1 className="text-3xl font-bold mb-4">About Page</h1>
+      <h1 className="text-3xl font-bold mb-4">About us:</h1>
       
       <div className="flex space-x-4">
         <div className="flex-1 mr-4">
           <h2 className="text-xl font-bold mb-2">Our Story</h2>
-          <p>We are two teenagers from London who decided it would be funny to code an unnecessarily complicated website. Our journey began with a shared passion for technology and a desire to challenge ourselves with a project that goes beyond the ordinary.</p>
-          <p>The idea of creating a website that is intentionally complex yet amusing quickly became our mission. From intricate coding puzzles to playful design elements, we strive to keep our audience entertained while showcasing our technical skills.</p>
-          <p>Join us on this whimsical coding adventure, where complexity meets creativity!</p>
+          <p>We are two teenagers from London who decided it would be funny to code an unnecessarily complicated website. Our journey began with a shared passion for technology and a desire to make our lives more difficult, with a project that goes beyond the neccessary amounts of work.</p>
+          <p>The idea of creating a website that is intentionally difficult to make (for two kids that is) yet not even that funny anymore, quickly became our mission. From intricate coding puzzles to literally wanting to restart the Union of Soviet Socialist Republics, we strive to keep our audience entertained while showcasing Pookie's Coding skills.</p>
+          <p>Join us on this traumatising experience, where coding meets yet more coding.</p>
         </div>
 
         <div className="flex-1 mr-4">
@@ -29,7 +29,7 @@ const About = () => {
           <h2 className="text-xl font-bold mb-2">Behind the Scenes</h2>
           <p>Get a glimpse into the making of our website:</p>
           <p>Countless hours of coding, testing, and refining went into crafting the intricate web pages you see. From late-night brainstorming sessions to overcoming unexpected challenges, our journey was filled with excitement and learning.</p>
-          <p>We hope you enjoy exploring the website as much as we enjoyed creating it. Stay tuned for future updates as we continue to add more layers to this digital masterpiece!</p>
+          <p>We hope you enjoy exploring the website as much as we hated creating it. Stay tuned for future updates as we continue to add more layers to this annoying piece of crap. </p>
         </div>
       </div>
     </div>
